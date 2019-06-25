856de50d38d5797b737b1b8934533b90d646f516
Argensik <d.legizynski@gmail.com>
2019-06-25 09:23:35 +0200

    Added jump and basic movement
    Basic movement still needs to be fixed since the player is not walking in the face direction.

--- Zenva_Constructing_FPS/Assets/Scripts/Player.cs
+++ Zenva_Constructing_FPS/Assets/Scripts/Player.cs
@@ -11,9 +11,18 @@ public class Player : MonoBehaviour
     [Header("Camera")]
     public float lookSensitivity;           //mouse look sensitivity
     public float maxLookX;                  //lowest we can look down
-    public float miLookY;                   //highest we can look up
-    private float rotX;                     //current x rotation of the camera
+    public float minLookX;                   //highest we can look up
+    float rotX;                     //current x rotation of the camera
 
+    Camera cam;
+    Rigidbody rig;
+
+    private void Awake()
+    {
+        //get the components
+        cam = Camera.main;
+        rig = GetComponent<Rigidbody>();
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +33,39 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+
+        if (Input.GetButtonDown("Jump"))
+            JumpFunction();
+
+        CamLook();
+    }
+
+    void Move()
+    {
+        float x = Input.GetAxis("Horizontal") * moveSpeed;
+        float z = Input.GetAxis("Vertical") * moveSpeed;
+
+        rig.velocity = new Vector3(x, rig.velocity.y, z);
+    }
+
+    void CamLook()
+    {
+        float y = Input.GetAxis("Mouse X") * lookSensitivity;
+        rotX += Input.GetAxis("Mouse Y") * lookSensitivity;
+
+        rotX = Mathf.Clamp(rotX, minLookX, maxLookX);
+
+        cam.transform.localRotation = Quaternion.Euler(-rotX, 0, 0);
+
+        transform.eulerAngles += Vector3.up * y;
+    }
+
+    void JumpFunction()
+    {
+        Ray ray = new Ray(transform.position, Vector3.down);
+
+        if (Physics.Raycast(ray, 1.1f))
+            rig.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
     }
 }
