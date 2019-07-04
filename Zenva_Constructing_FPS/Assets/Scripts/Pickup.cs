a526284b7a596f819ad3d32352c1bd7ef4e3a349
Argensik <d.legizynski@gmail.com>
2019-07-04 16:18:44 +0200

    Made pickups

--- Zenva_Constructing_FPS/Assets/Scripts/Pickup.cs
+++ Zenva_Constructing_FPS/Assets/Scripts/Pickup.cs
@@ -10,5 +10,35 @@ public enum PickupType
 
 public class Pickup : MonoBehaviour
 {
-    
+    public PickupType type;
+    public int value;
+
+    [Header("Bobbing")]
+    public float rotateSpeed;
+    public float bobSpeed;
+    public float bobHeight;
+
+    Vector3 startPos;
+    bool bobbingUp;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Player player = other.GetComponent<Player>();
+
+            switch (type)
+            {
+                case PickupType.Health:
+                    player.GiveHealth(value);
+                    break;
+
+                case PickupType.Ammo:
+                    player.GiveAmmo(value);
+                    break;
+            }
+
+            Destroy(gameObject);
+        }
+    }
 }
